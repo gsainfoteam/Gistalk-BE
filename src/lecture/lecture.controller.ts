c94e7c050f7fe5caa864927f10aa32d218774268
minjunj <jamman331@gmail.com>
2023-02-23 21:17:48 +0900

    강의 코드로 강의 ID 조회 API

--- src/lecture/lecture.controller.ts
+++ src/lecture/lecture.controller.ts
@@ -8,18 +8,25 @@ import { LectureService } from './lecture.service';
 export class LectureController {
   constructor(private readonly lectureService: LectureService) {}
 
+  //강의 코드로 강의 아이디 조회 API
+  @Get('getting:lecture_code')
+  getLectureId(@Param('lecture_code') lecture_code: string): Promise<any> {
+    return this.lectureService.getLectureId(lecture_code)
+  }
+
   //강좌별 강의 평가 조회 API
   @Get('get:lecture_id')
   getLectureInfo(@Param('lecture_id') lecture_id: number): Promise<any> {
     return this.lectureService.getLectureInfo(lecture_id);
   }
 
-  //강의추가
+  //강의추가 API
   @Post('/add')
   createProfLecture(@Body() lectureData: CreateLectureDto): Promise<Lecture> {
     return this.lectureService.createProfLecture(lectureData);
   }
 
+  //강의 삭제 API
   @Delete("delete:id")
   DeleteLecture(@Param('id') id : number): Promise<any>
   {
