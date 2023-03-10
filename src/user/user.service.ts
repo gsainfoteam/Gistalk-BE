ad6e2452653cfa6dcfcb1a9f2d72e975d262ab3a
minjunj <jamman331@gmail.com>
2023-03-11 02:09:30 +0900

    idp connect done - need to set the CLIENT_SECRET_KEY .env

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,31 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly httpService: HttpService) {}
+
+    /**idp로 부터 get 요청을 통해 유저의 로그인 여부를 확인합니다.*/
+    async LogIn(jwt_token : string): Promise<any>
+    {
+        const token = JSON.stringify(jwt_token).split('"')[3];
+        console.log(typeof(token));
+        let data = 'api.idp.gistory.me/idp/get_user_info'
+
+        const ani = await this.httpService.axiosRef.get(
+            'https://api.idp.gistory.me/idp/get_user_info',     
+            {
+                params : {
+                    jwt_token : token,
+                    client_id : 'gistalk2023',
+                    client_secret_key : 'secret_key in CLINET_SECRET_KEY ./env'
+                }
+            }
+        );
+        console.log(ani)
+        return ani.data
+    }  
+
+}
+
