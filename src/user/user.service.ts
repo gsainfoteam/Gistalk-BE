4e820b6f85e3b80901f681b1903ee995d9cdc79a
minjunj <jamman331@gmail.com>
2023-03-11 02:25:17 +0900

    uer login API 예외처리

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class UserService {
@@ -13,18 +14,25 @@ export class UserService {
         console.log(typeof(token));
         let data = 'api.idp.gistory.me/idp/get_user_info'
 
-        const ani = await this.httpService.axiosRef.get(
-            'https://api.idp.gistory.me/idp/get_user_info',     
-            {
-                params : {
-                    jwt_token : token,
-                    client_id : 'gistalk2023',
-                    client_secret_key : 'secret_key in CLINET_SECRET_KEY ./env'
+        try{
+            const ani = await this.httpService.axiosRef.get(
+                'https://api.idp.gistory.me/idp/get_user_info',     
+                {
+                    params : {
+                        jwt_token : token,
+                        client_id : 'gistalk2023',
+                        client_secret_key : 'HDRbyP277JND7YbZCKINM92M3vi7BALx'
+                    }
                 }
-            }
-        );
-        console.log(ani)
-        return ani.data
+            );
+
+            return ani.data
+        } catch(err){
+            console.log(err)
+            throw new NotFoundException('등록되지않은 유저입니다.');
+        }
+
+        
     }  
 
 }
