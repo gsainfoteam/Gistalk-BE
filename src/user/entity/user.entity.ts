92fd8c69f72d84a27979abfd997a0862b00a64e9
minjunj <jamman331@gmail.com>
2023-03-22 21:53:39 +0900

    gitalk's accessToken 발급 & idp cliend_id, secet_key .env화

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -3,12 +3,19 @@ import {
   Column,
   ManyToOne,
   PrimaryColumn,
+  Binary,
+  PrimaryGeneratedColumn,
+  Unique,
 } from 'typeorm';
 
 @Entity()
+@Unique(['email'])
 export class User {
   @PrimaryColumn()
-  uuid: string; // uuid
+  uuid : string;
+
+  @Column()
+  email : string;
 
   //1:n 관계로 강의평 엔티티 맵핑.
 
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,28 @@ import { UserController } from './user.controller';
 import { HttpModule } from '@nestjs/axios';
 import { User } from './entity/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [HttpModule, TypeOrmModule.forFeature([User])],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        CLIENT_ID: Joi.string().required(),
+        CLIENT_SECRET_KEY: Joi.string().required(),
+      }),
+    }),
+    PassportModule.register({defaultStrategy: 'jwt'})
+    ,JwtModule.register({
+      secret:'gistalk2023',
+      signOptions:{
+        expiresIn: '3h',// '10y'
+      }
+    })
+    ,HttpModule, TypeOrmModule.forFeature([User])],
   providers: [UserService],
   controllers: [UserController]
 })
