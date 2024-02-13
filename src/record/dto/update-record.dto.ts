a35ae8d751bfebc6389b7a885e8e81461f21e50c
minjunj <minjun_jo@gm.gist.ac.kr>
2024-02-13 20:16:33 +0900

    record-update api & scoring bug fix : scoring/get

---
+++ src/record/dto/update-record.dto.ts
@@ -0,0 +1,62 @@
+import {
+  IsBoolean,
+  IsInt,
+  IsNumberString,
+  IsPositive,
+  Length,
+  Max,
+} from 'class-validator';
+
+export class UpdateRecordDto {
+  @IsInt()
+  @IsPositive()
+  @Max(5)
+  readonly difficulty: number;
+
+  @IsInt()
+  @IsPositive()
+  @Max(5)
+  readonly strength: number;
+
+  @IsInt()
+  @IsPositive()
+  @Max(5)
+  readonly helpful: number;
+
+  @IsInt()
+  @IsPositive()
+  @Max(5)
+  readonly interest: number;
+
+  @IsInt()
+  @IsPositive()
+  @Max(5)
+  readonly lots: number;
+
+  @IsInt()
+  @IsPositive()
+  @Max(5)
+  readonly satisfy: number;
+
+  @Length(15, 255)
+  readonly review: string;
+
+  @IsInt()
+  @IsPositive()
+  readonly lecture_id: number;
+
+  @IsInt()
+  @IsPositive()
+  readonly prof_id: number;
+
+  @IsInt()
+  @IsPositive()
+  @Max(4)
+  readonly semester_id: number;
+
+  @IsNumberString()
+  @Length(4, 4)
+  readonly year: string;
+
+  readonly recommend?: number;
+}
